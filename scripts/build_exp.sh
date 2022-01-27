5401395be6741623a3f6edecb98d01953fd76d40
SasoriZero <Mandaloe2@gmail.com>
2022-01-26 23:32:15 -0800

    Add Debug Build
    mtlpp.o and build_exp.sh

---
+++ scripts/build_exp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+build_experiment()
+{
+    local ver=$1
+    local output="../build/macos_$ver"
+    local objcflags="-std=c++11 -x objective-c++ -mmacosx-version-min=$ver"
+    local cppflags="-std=c++11 -mmacosx-version-min=$ver"
+    local ldflags="-framework Metal -framework MetalKit -framework Cocoa -framework CoreFoundation -fobjc-link-runtime"
+
+    rm -Rf $output
+    mkdir -p $output
+
+    clang++ $objcflags -c ../mtlpp.mm -o $output/mtlpp.o
+    clang++ $cppflags $ldflags ../mtl++/main.cpp $output/mtlpp.o -o $output/metalAdder
+}
+
+    build_experiment 10.12
+    echo "Complete Build: Experiment"
+
