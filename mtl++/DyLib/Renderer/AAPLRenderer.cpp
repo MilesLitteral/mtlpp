34a1ca7e52ccca970009b87c7dcea2e7f0c0b021
SasoriZero <Mandaloe2@gmail.com>
2022-01-27 22:31:12 -0800

    Update AAPLRenderer.cpp

--- mtl++/DyLib/Renderer/AAPLRenderer.cpp
+++ mtl++/DyLib/Renderer/AAPLRenderer.cpp
@@ -6,27 +6,28 @@ Implementation of the renderer class that performs Metal setup and per-frame ren
 */
 
 #include <simd/simd.h>
-#include <Metal/MetalKit.h>
-#include <../../../mtlpp.hpp>
-#import "AAPLRenderer.h"
-#import "AAPLMathUtilities.h"
+#include <MetalKit/MetalKit.h>
+
+#include "AAPLRenderer.h"
+#include "AAPLMathUtilities.h"
 
 // Include header shared between C code here, which executes Metal API commands, and .metal files
-#import "AAPLShaderTypes.h"
+#include "AAPLShaderTypes.h"
+#include "mtlpp.hpp"
 
 // The max number of command buffers in flight
 static const int AAPLMaxFramesInFlight = 3;
 
-
 // Main class performing the rendering
 class AAPLRenderer
 {
-    dispatch_semaphore_t _inFlightSemaphore;
-    mtlpp::Buffer _frameDataBuffer = AAPLMaxFramesInFlight();
-    NSUInteger _frameNumber;
-
-    mtlpp::Device _device;
+    mtlpp::Device _device = mtlpp::Device::CreateSystemDefaultDevice();
     mtlpp::CommandQueue _commandQueue;
+    mtlpp::CommandBuffer _commandBuffer = _commandQueue.CommandBuffer();
+
+    //dispatch_semaphore_t _inFlightSemaphore;
+    mtlpp::Buffer _frameDataBuffer = _device.NewBuffer(AAPLMaxFramesInFlight, mtlpp::ResourceOptions::StorageModeManaged);
+    int _frameNumber;
 
     mtlpp::RenderPipelineState _renderPipeline;
     mtlpp::DepthStencilState _depthState;
@@ -36,9 +37,9 @@ class AAPLRenderer
     mtlpp::Buffer _indexBuffer;
     mtlpp::Texture _colorTarget;
 
-    MTLRenderPassDescriptor *_renderPassDescriptor;
+    mtlpp::RenderPassDescriptor *_renderPassDescriptor;
 
-    MTLVertexDescriptor *_vertexDescriptor;
+    mtlpp::VertexDescriptor *_vertexDescriptor;
     // Projection matrix calculated as a function of view size
     matrix_float4x4 _projectionMatrix;
     float _rotation;
@@ -48,103 +49,98 @@ class AAPLRenderer
     mtlpp::Size _dispatchExecutionSize;
     mtlpp::Size _threadsPerThreadgroup;
     int _threadgroupMemoryLength;
-}
 
-/// Initialize with the MetalKit view with the Metal device used to render.  This MetalKit view
-/// object will also be used to set the pixelFormat and other properties of the drawable
-class initWithMetalKitView
-{
-    //self = [super init];
-    initWithMetalKitView(MTKView * mtkView)
-    {
-        _device = mtkView.device;
-        _inFlightSemaphore = dispatch_semaphore_create(AAPLMaxFramesInFlight);
-        mtlpp::loadMetal(mtkView);
-        mtlpp::loadAssets();
-    }
-    
-    return self;
-}
+
+// Initialize with the MetalKit view with the Metal device used to render.  This MetalKit view
+// object will also be used to set the pixelFormat and other properties of the drawable
+// class initWithMetalKitView
+// {
+//     initWithMetalKitView(MTKView* mtkView)
+//     {
+//         _device = mtkView.device;
+//         _inFlightSemaphore = dispatch_semaphore_create(AAPLMaxFramesInFlight);
+//         mtkView.loadMetal();
+//         MTK::loadAssets;
+//     }
+// };
 
 /// Create the Metal render state objects including shaders and render state pipeline objects
-- (void)loadMetal:(nonnull MTKView*)mtkView
-{
-    [self setUpView:mtkView];
+// void loadMetal(MTKView* mtkView)
+// {
+//     mtkView.setUpView();
 
-    id<MTLLibrary> metalLibrary = [self loadMetallib];
+//     mtlpp::Library metalLibrary = [self loadMetallib];
 
-    [self createBuffers];
+//     [self createBuffers];
 
-    [self createRenderStateWithLibrary:metalLibrary];
+//     [self createRenderStateWithLibrary:metalLibrary];
 
-    [self createComputePipelineWithLibrary:metalLibrary];
+//     [self createComputePipelineWithLibrary:metalLibrary];
 
-    _commandQueue = [_device newCommandQueue];
-}
+//     _commandQueue = [_device newCommandQueue];
+// }
 
 // Load the Metal library created in the "Build Executable Metal Library" build phase.
 // This library includes the functions in AAPLShaders.metal and the UserDylib.metallib
 // created in the "Build Dynamic Metal Library" build phase.
-- (id<MTLLibrary>)loadMetallib
+mtlpp::Library loadMetallib(mtlpp::Device _device)
 {
-    NSError *error;
+    ns::Error *error;
 
-    id<MTLLibrary> library = [_device newLibraryWithURL:[[NSBundle mainBundle]
-                                                                URLForResource:@"AAPLShaders"
-                                                                withExtension:@"metallib"]
-                                                         error:&error];
+    mtlpp::Library library = _device.NewLibrary("AAPLShaders.metallib", error);
 
-    NSAssert(library, @"Failed to load AAPLShaders dynamic metal library: %@", error);
+    assert(library);
+    if(library == NULL){
+        printf("Failed to load AAPLShaders dynamic metal library: %@", error);
+    }
 
     return library;
 }
 
-/// Set up properties of the view
-- (void)setUpView:(nonnull MTKView*)mtkView
-{
-    mtkView.colorPixelFormat = MTLPixelFormatBGRA8Unorm;
-    mtkView.sampleCount = 1;
-}
+// // Set up properties of the view
+// void setUpView(MTKView* mtkView)
+// {
+//     mtkView.colorPixelFormat = MTLPixelFormatBGRA8Unorm;
+//     mtkView.sampleCount = 1;
+// }
 
-/// Create buffers modified each frame to animate the cube
-- (void)createBuffers
+// Create buffers modified each frame to animate the cube
+void createBuffers()
 {
-    for(NSUInteger i = 0; i < AAPLMaxFramesInFlight; i++)
+    for(int i = 0; i < AAPLMaxFramesInFlight; i++)
     {
-        _frameDataBuffer[i] = [_device newBufferWithLength:sizeof(AAPLPerFrameData)
-                                                   options:MTLResourceStorageModeShared];
-
-        _frameDataBuffer[i].label = @"FrameDataBuffer";
+        _frameDataBuffer[i] = _device.NewBuffer(sizeof(AAPLPerFrameData), mtlpp::ResourceOptions::StorageModeShared);
+        _frameDataBuffer[i].label = "FrameDataBuffer";
     }
 }
 
 /// Create the render pass descriptor, render pipeline state object, and depth state object to render the cube.
-- (void)createRenderStateWithLibrary:(id<MTLLibrary>)metallib
+void createRenderStateWithLibrary(mtlpp::Library metallib)
 {
-    NSError *error;
+    ns::Error *error;
 
     // Set up render pass descriptor
     {
-        _renderPassDescriptor = [MTLRenderPassDescriptor new];
-        _renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 1);
-        _renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
-        _renderPassDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
+        _renderPassDescriptor = new mtlpp::RenderPassDescriptor();
+        _renderPassDescriptor->GetColorAttachments()[0].SetClearColor(mtlpp::ClearColor(0, 0, 0, 1));
+        _renderPassDescriptor->GetColorAttachments()[0].SetLoadAction(mtlpp::LoadAction::Clear); // LoadActionClear);
+        _renderPassDescriptor->GetColorAttachments()[0].SetStoreAction(mtlpp::StoreAction::Store);
 
-        _renderPassDescriptor.depthAttachment.loadAction = MTLLoadActionClear;
-        _renderPassDescriptor.depthAttachment.storeAction = MTLStoreActionDontCare;
+        _renderPassDescriptor->GetDepthAttachment().SetLoadAction(mtlpp::LoadAction::Clear);
+        _renderPassDescriptor->GetDepthAttachment().SetStoreAction(mtlpp::StoreAction::DontCare);
     }
 
     // Set up render pipeline
     {
-        _vertexDescriptor = [MTLVertexDescriptor new];
+        _vertexDescriptor = new mtlpp::VertexDescriptor();
     
-        _vertexDescriptor.attributes[AAPLVertexAttributePosition].format = MTLVertexFormatFloat3;
-        _vertexDescriptor.attributes[AAPLVertexAttributePosition].offset = 0;
-        _vertexDescriptor.attributes[AAPLVertexAttributePosition].bufferIndex = AAPLBufferIndexMeshPositions;
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributePosition].SetFormat(mtlpp::VertexFormat::Float3);
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributePosition].SetOffset(0);
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributePosition].bufferIndex = AAPLBufferIndexMeshPositions;
 
-        _vertexDescriptor.attributes[AAPLVertexAttributeTexcoord].format = MTLVertexFormatFloat2;
-        _vertexDescriptor.attributes[AAPLVertexAttributeTexcoord].offset = 0;
-        _vertexDescriptor.attributes[AAPLVertexAttributeTexcoord].bufferIndex = AAPLBufferIndexMeshGenerics;
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributeTexcoord].SetFormat(mtlpp::VertexFormat::Float2);
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributeTexcoord].offset = 0;
+        _vertexDescriptor->GetAttributes()[AAPLVertexAttributeTexcoord].bufferIndex = AAPLBufferIndexMeshGenerics;
 
         _vertexDescriptor.layouts[AAPLBufferIndexMeshPositions].stride = 16;
         _vertexDescriptor.layouts[AAPLBufferIndexMeshPositions].stepRate = 1;
@@ -155,13 +151,13 @@ class initWithMetalKitView
         _vertexDescriptor.layouts[AAPLBufferIndexMeshGenerics].stepFunction = MTLVertexStepFunctionPerVertex;
 
         // Load the vertex function from the library
-        id<MTLFunction> vertexFunction = [metallib newFunctionWithName:@"vertexShader"];
+        mtlpp::Function vertexFunction = metallib.NewFunction("vertexShader");
 
         // Load the fragment function from the library
-        id<MTLFunction> fragmentFunction = [metallib newFunctionWithName:@"fragmentShader"];
+        mtlpp::Function fragmentFunction = metallib.NewFunction("fragmentShader");
 
-        MTLRenderPipelineDescriptor *pipelineDescriptor = [MTLRenderPipelineDescriptor new];
-        pipelineDescriptor.label = @"RenderPipeline";
+        mtllpp::RenderPipelineDescriptor *pipelineDescriptor =   new mtlpp::RenderPipelineDescriptor();
+        pipelineDescriptor.label = "RenderPipeline";
         pipelineDescriptor.sampleCount = 1;
         pipelineDescriptor.vertexFunction = vertexFunction;
         pipelineDescriptor.vertexDescriptor = _vertexDescriptor;
@@ -169,42 +165,44 @@ class initWithMetalKitView
         pipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormatRGBA8Unorm;
         pipelineDescriptor.depthAttachmentPixelFormat = MTLPixelFormatDepth32Float;
 
-        _renderPipeline = [_device newRenderPipelineStateWithDescriptor:pipelineDescriptor error:&error];
+        _renderPipeline = _device.NewRenderPipelineStateWithDescriptor(pipelineDescriptor, error);
 
-        NSAssert(_renderPipeline, @"Failed to create render pipeline state: %@", error);
+        assert(_renderPipeline) 
+        if(_renderPipeline == NULL){
+            printf("Failed to create render pipeline state: %@", error);
+        }
     }
 
     // Set up depth stencil state
     {
-        MTLDepthStencilDescriptor *depthStateDesc = [MTLDepthStencilDescriptor new];
-        depthStateDesc.depthCompareFunction = MTLCompareFunctionLess;
-        depthStateDesc.depthWriteEnabled = YES;
+        mtlpp::DepthStencilDescriptor *depthStateDesc = new mtlpp::DepthStencilDescriptor();
+        depthStateDesc.depthCompareFunction = mtlpp::CompareFunction(CompareFunction::Less);
+        depthStateDesc.depthWriteEnabled = true;
         _depthState = [_device newDepthStencilStateWithDescriptor:depthStateDesc];
     }
 }
 
-- (void)createComputePipelineWithLibrary:(id<MTLLibrary>)metallib
+void createComputePipelineWithLibrary(mtlpp::Library metallib)
 {
-    NSError *error;
+    ns::Error *error;
 
-    id<MTLFunction> kernelFunction = [metallib newFunctionWithName:@"dylibKernel"];
-    _baseDescriptor = [MTLComputePipelineDescriptor new];
-    _baseDescriptor.computeFunction = kernelFunction;
-    MTLComputePipelineDescriptor *descriptor = [MTLComputePipelineDescriptor new];
+    mtlpp::Function kernelFunction = metallib.NewFunction("dylibKernel");
+    _baseDescriptor = new mtlpp::ComputePipelineDescriptor();
+    _baseDescriptor->SetComputeFunction(kernelFunction);
+    mtlpp::ComputePipelineDescriptor *descriptor = new mtlpp::ComputePipelineDescriptor();
     descriptor.computeFunction = _baseDescriptor.computeFunction;
     
-    _computePipeline = [_device newComputePipelineStateWithDescriptor:descriptor
-                                                              options:MTLPipelineOptionNone
-                                                           reflection:nil
-                                                                error:&error];
-    NSAssert(_computePipeline, @"Error creating pipeline which links library from source: %@", error);
+    _computePipeline = _device.newComputePipelineState(descriptor, options:MTLPipelineOptionNone, NULL, error);
+    assert(_computePipeline)
+    if(_computePipeline == NULL){
+        printf("Error creating pipeline which links library from source: %@", error);
+    }
 }
 
 /// Load assets into metal objects
-- (void)loadAssets
+void loadAssets()
 {
     // Create a buffer with positions to draw the cube.
-    {
         static const vector_float3 cubePositions[] =
         {
             // Front
@@ -244,13 +242,9 @@ class initWithMetalKitView
             { -1, -1,  1 }
         };
 
-        _positionBuffer = [_device newBufferWithBytes:cubePositions
-                                               length:sizeof(cubePositions)
-                                              options:0];
-    }
+        _positionBuffer = _device.NewBuffer(cubePositions, sizeof(cubePositions), 0);
 
     // Create a buffer with texture coordinates to draw the cube.
-    {
         static const vector_float2 cubeTexCoords[] =
         {
             // Front
@@ -290,14 +284,9 @@ class initWithMetalKitView
             { 1, 0 },
         };
 
-        _texCoordBuffer = [_device newBufferWithBytes:cubeTexCoords
-                                               length:sizeof(cubeTexCoords)
-                                              options:0];
-    }
+        _texCoordBuffer = _device.NewBuffer(cubeTexCoords, sizeof(cubeTexCoords), 0);
 
     // Create the index buffer to draw the cube.
-    {
-
         static uint16_t indices[] =
         {
             // Front
@@ -319,22 +308,19 @@ class initWithMetalKitView
             20, 22, 21, 20, 23, 22,
         };
 
-        _indexBuffer = [_device newBufferWithBytes:indices
-                                            length:sizeof(indices)
-                                           options:0];
-    }
+        _indexBuffer = _device.newBuffer(indices, sizeof(indices), 0);
 
     // Load color texture from asset catalog
     {
         MTKTextureLoader* textureLoader = [[MTKTextureLoader alloc] initWithDevice:_device];
 
-        NSDictionary *textureLoaderOptions =
-        @{
+        enum *textureLoaderOptions =
+        {
             MTKTextureLoaderOptionTextureUsage       : @(MTLTextureUsageShaderRead),
             MTKTextureLoaderOptionTextureStorageMode : @(MTLStorageModePrivate)
-         };
+        };
 
-        NSError *error;
+        ns::Error *error;
 
         _colorMap = [textureLoader newTextureWithName:@"ColorMap"
                                           scaleFactor:1.0
@@ -342,12 +328,15 @@ class initWithMetalKitView
                                               options:textureLoaderOptions
                                                 error:&error];
 
-        NSAssert(_colorMap, @"Error creating the Metal texture, error: %@.", error);
+        assert(_colorMap) 
+        if(_colorMap == NULL){
+            printf(@"Error creating the Metal texture, error: %@.", error);
+        }
     }
 }
 
-/// Update any scene state before encoding rendering commands to our drawable
-- (void)updateSceneState
+//Update any scene state before encoding rendering commands to our drawable
+void updateSceneState()
 {
     NSUInteger frameDataBufferIndex = _frameNumber % AAPLMaxFramesInFlight;
 
@@ -495,7 +484,7 @@ class initWithMetalKitView
 }
 
 /// Compile a dylib with the given program string then create a compute pipeline with the dylib
--(void)compileDylibWithString:(NSString *)programString
+void compileDylibWithString:(NSString * programString)
 {
     NSError *error;
 
@@ -503,12 +492,12 @@ class initWithMetalKitView
     options.libraryType = MTLLibraryTypeDynamic;
     options.installName = [NSString stringWithFormat:@"@executable_path/userCreatedDylib.metallib"];
 
-    id<MTLLibrary> lib = [_device newLibraryWithSource:programString
+    mtlpp::Library> lib = [_device newLibraryWithSource:programString
                                                options:options
                                                  error:&error];
     if(!lib && error)
     {
-        NSLog(@"Error compiling library from source: %@", error);
+        printf("Error compiling library from source: %@", error);
         return;
     }
     
@@ -516,25 +505,22 @@ class initWithMetalKitView
                                                             error:&error];
     if(!dynamicLib && error)
     {
-        NSLog(@"Error creating dynamic library from source library: %@", error);
+        printf("Error creating dynamic library from source library: %@", error);
         return;
     }
     
-    MTLComputePipelineDescriptor *descriptor = [MTLComputePipelineDescriptor new];
+    mtlpp::ComputePipelineDescriptor *descriptor = [MTLComputePipelineDescriptor new];
     descriptor.computeFunction = _baseDescriptor.computeFunction;
     descriptor.insertLibraries = @[dynamicLib];
     
-    id<MTLComputePipelineState> previousComputePipeline = _computePipeline;
-    _computePipeline = [_device newComputePipelineStateWithDescriptor:descriptor
-                                                              options:MTLPipelineOptionNone
-                                                           reflection:nil
-                                                                error:&error];
-    if(!_computePipeline && error)
-    {
-        NSLog(@"Error creating pipeline library from source library, using previous pipeline: %@", error);
-        _computePipeline = previousComputePipeline;
-        return;
+    mtlpp::ComputePipelineState previousComputePipeline = _computePipeline;
+    _computePipeline = _device.newComputePipelineStateWithDescriptor(descriptor, MTLPipelineOptionNone, NULL, error);
+        if(!_computePipeline && error)
+        {
+            printf("Error creating pipeline library from source library, using previous pipeline: %@", error);
+            _computePipeline = previousComputePipeline;
+            return;
+        }
     }
 }
 
-@end
